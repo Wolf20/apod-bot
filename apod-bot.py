12b367dee2e6f86f6c9ff34f39618a193ac7c28d
Arturo Herrero <arturo@syngularity.es>
2020-10-08 10:27:42 +0200

    Revert "added translation  function to all outputs, i think this should work, I cannot test it myself."
    This reverts commit c70fb3d9d82248cdeb11b579d5d88bb520a17f94.

--- apod-bot.py
+++ apod-bot.py
@@ -1,16 +1,9 @@
 import telebot
 import requests
 import json
-from googletrans import Translator as trans
 from credentials import *
 
 bot = telebot.TeleBot(tg_key)
-def t(text, dest, src):
-    text= str(text)
-    dest = str(dest)
-    src= str(src)  
-    text = translator.translate(text, dest=(dest),src=src)
-    return text.text
 
 def main():
     payload = { 'api_key' : nasa_key}
@@ -18,15 +11,14 @@ def main():
     response = r.content
     json2py = json.loads(response)
     if json2py['media_type'] == "video":
-        reply = t(f"Today's APOD is [{json2py['title']}]({json2py['url']}).\n\n{json2py['explanation']}","es","en")
+        reply = f"Today's APOD is [{json2py['title']}]({json2py['url']}).\n\n{json2py['explanation']}"
         bot.send_message(chat_id, reply, parse_mode='markdown')
     else:
         if 'copyright' not in json2py:
-            reply = (t(f"Today's APOD is [{json2py['title']}]","es","en")+"f({json2py['url']})"+t("\n\n{json2py['explanation']}\n\nCheck out HD picture [here]({json2py['hdurl']}).","es","en"))
+            reply = f"Today's APOD is [{json2py['title']}]({json2py['url']}).\n\n{json2py['explanation']}\n\nCheck out HD picture [here]({json2py['hdurl']})."
         else:
-            reply = t(f"Today's APOD is [{json2py['title']}]({json2py['url']}).\n\n{json2py['explanation']}\n\nCheck out HD picture [here]({json2py['hdurl']}).\n\nImage Credit & Copyright: {json2py['copyright']}.","es","en")
+            reply = f"Today's APOD is [{json2py['title']}]({json2py['url']}).\n\n{json2py['explanation']}\n\nCheck out HD picture [here]({json2py['hdurl']}).\n\nImage Credit & Copyright: {json2py['copyright']}."
         bot.send_message(chat_id, reply, parse_mode='markdown')
 
 if __name__ == '__main__':
-    translator = trans()
     main()
