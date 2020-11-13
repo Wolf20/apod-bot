2691b5148c48f07849fffdcd22c930e078a85abf
EdgarMontelongo <55753336+EdgarMontelongo@users.noreply.github.com>
2020-11-13 08:31:39 -0600

    added looping in case of connection error.
    the above

--- apod-bot.py
+++ apod-bot.py
@@ -1,8 +1,10 @@
 import telebot
 import requests
 import json
+import time as tiempo
 from googletrans import Translator
 from credentials import *
+
 
 #Init some variables and functions
 bot = telebot.TeleBot(tg_key)
@@ -11,41 +13,47 @@ translate = Translator().translate
 lang = 'es'
 
 def main():
-    payload = { 'api_key' : nasa_key}
-    r = requests.get(BASE_URL, params = payload)
-    response = r.content
-    json2py = json.loads(response)
-    if lang == "en":
-        intro = f"Today's APOD is"
-    elif lang == "es":
-        intro = f"El APOD de hoy es"
-    else:
-        intro = translate(f"Today's APOD is", dest=lang).text
-    title = translate(f"{json2py['title']})", dest=lang).text
-    url = f"{json2py['url']}"
-    explanation = translate(f"{json2py['explanation']}", dest=lang).text
-    if lang == "en":
-        outro = f"Check out HD picture [here]"
-    elif lang == "es":
-        outro = f"Mira la imagen en HD [aqui]"
-    else:
-        outro = translate(f"Check out HD picture [here]", dest=lang).text
-    if lang == "en":
-        coutro = f"Image Credit & Copyright"
-    elif lang == "es":
-        coutro = f"Creditos y Copyright de la Imagen"
-    else:
-        coutro = translate(f"Image Credit & Copyright", dest=lang).text
-    if json2py['media_type'] == "video":
-        teleapod = intro + " [" + title + "](" + url + ")" + ".\n\n" + explanation
-    else:
-        hdurl = f"{json2py['hdurl']}"
-        if 'copyright' not in json2py:
-            teleapod = intro + " [" + title + "](" + url + ").\n\n" + explanation + "\n\n" + outro + "(" + hdurl + ")."
+    try:
+        payload = { 'api_key' : nasa_key}
+        r = requests.get(BASE_URL, params = payload)
+        response = r.content
+        json2py = json.loads(response)
+        if lang == "en":
+            intro = f"Today's APOD is"
+        elif lang == "es":
+            intro = f"El APOD de hoy es"
         else:
-            imgrights = f"{json2py['copyright']}"
-            teleapod = intro + " [" + title + "](" + url + ").\n\n" + explanation + "\n\n" + outro + "(" + hdurl + ").\n\n" + coutro + ": " + imgrights + "."
-    telepost(chat_id, teleapod, parse_mode='markdown')
+            intro = translate(f"Today's APOD is", dest=lang).text
+        title = translate(f"{json2py['title']})", dest=lang).text
+        url = f"{json2py['url']}"
+        explanation = translate(f"{json2py['explanation']}", dest=lang).text
+        if lang == "en":
+            outro = f"Check out HD picture [here]"
+        elif lang == "es":
+            outro = f"Mira la imagen en HD [aqui]"
+        else:
+            outro = translate(f"Check out HD picture [here]", dest=lang).text
+        if lang == "en":
+            coutro = f"Image Credit & Copyright"
+        elif lang == "es":
+            coutro = f"Creditos y Copyright de la Imagen"
+        else:
+            coutro = translate(f"Image Credit & Copyright", dest=lang).text
+        if json2py['media_type'] == "video":
+            teleapod = intro + " [" + title + "](" + url + ")" + ".\n\n" + explanation
+        else:
+            hdurl = f"{json2py['hdurl']}"
+            if 'copyright' not in json2py:
+                teleapod = intro + " [" + title + "](" + url + ").\n\n" + explanation + "\n\n" + outro + "(" + hdurl + ")."
+            else:
+                imgrights = f"{json2py['copyright']}"
+                teleapod = intro + " [" + title + "](" + url + ").\n\n" + explanation + "\n\n" + outro + "(" + hdurl + ").\n\n" + coutro + ": " + imgrights + "."
+        telepost(chat_id, teleapod, parse_mode='markdown')
+    except AttributeError:
+        tiempo.sleep(10)
+        print( " No se ha podido contactar con los servidores de google, reintentando...")
+        main()
 
 if __name__ == '__main__':
-    main()
+        main()
+    
